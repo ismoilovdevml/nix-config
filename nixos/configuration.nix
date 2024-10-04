4d0f9ad30268e26e4d2b01f60ef6d71f9510ed65
ismoilovdevml <ismoilovdevarchlinux@gmail.com>
2024-10-04 21:57:21 +0500

    [FIX] microphone problem

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -11,13 +11,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
   # Set your time zone.
@@ -56,65 +49,43 @@
 
   # Enable sound with pipewire.
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;  # Uncomment if needed
   };
+  
+  security.rtkit.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-   
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.ismoilovdev = {
     isNormalUser = true;
     description = "Otabek Ismoilov";
     extraGroups = [ "networkmanager" "wheel" "docker"];
     packages = with pkgs; [
-    #  thunderbird
        telegram-desktop
     ];
   };
 
   # Install firefox.
+  sound.enable = true;
   programs.firefox.enable = true;
   nixpkgs.config.allowUnfree = true;
   nixpkgs.config.permittedInsecurePackages = [
     "openssl-1.1.1w"
     "pulsar-1.117.0"
   ];
-  users.defaultUserShell = pkgs.zsh;
-  environment.shells = with pkgs; [ zsh ];
-  programs.zsh = {
-      enable = true;
-      ohMyZsh = {
-        enable = true;
-        plugins = [ "git" "zsh-autosuggestions" "zsh-syntax-highlighting" ];
-        theme = "powerlevel10k/powerlevel10k";
-      };
-    };
 
   virtualisation.docker.enable = true;
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+
   environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
     zsh
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    zsh-autocomplete
-    zsh-powerlevel10k
     neovim
     helix
+    nfs-utils
     nodejs_22
     pnpm
     go
@@ -168,31 +139,11 @@
     curl
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  # Disable the firewall.
+  networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
-
 }
